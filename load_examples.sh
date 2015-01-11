874e9057ec3a6da1f56608f4f3b01015f9defed3
Joshua Orvis <jorvis@gmail.com>
2015-01-10 22:58:50 -0600

    Added script to load an example database

---
+++ load_examples.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Create the database file.  If it already exists, stop
+file="$HOME/.qtask.db"
+if [ -f "$file" ]
+then
+	echo "The qtask file $file already exists.  Please remove or back-up before running this."
+    exit 1
+else
+	echo "$file not found - Initializing"
+    qtask init
+fi
+
+echo "\nCreating projects"
+qtask add project Annotation
+qtask add project 'T parva'
+
+echo "\nLogging work"
+qtask log "Group status meeting"
+qtask log "Configured JBrowse instance" to "T parva"
+qtask log "Created script to split isoforms" to "T parva"
+qtask log "Generated latest annotation" to "T parva"
+qtask log "Conference call with S. Smith"
+qtask log "Submitted work summary for the week"
+qtask log "Added EggNOG parsing script" to Annotation
+qtask log "Installated latest version of HMMER" to Annotation
+
+
+
+
+
+
+
+
+
